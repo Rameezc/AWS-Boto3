9539af1a21aa17a82324c6017e2658cf829717ce
Rameezc <49646929+Rameezc@users.noreply.github.com>
2019-04-19 15:46:28 +0200

    Update List-Lambda-Functions.py

--- List-Lambda-Functions.py
+++ List-Lambda-Functions.py
@@ -1,5 +1,6 @@
 # This program will list the all the Functions in each Region
-# I have 
+# I have permission related issues - So have removed the Region "ap-northeast-3"
+
 import boto3
 
 desc_regions = ec2.describe_regions()
@@ -7,14 +8,15 @@ desc_regions = ec2.describe_regions()
 for regions in desc_regions['Regions']:
 	region_name = regions['RegionName']
 	if region_name != "ap-northeast-3":
-		client = boto3.client('lambda', region_name=region_name) # you don't need to save to a list and then loop through a list of region names if you can do it here one time!
+		# You don't need to save to a list and then loop through a list of region names if you can do it here one time!
+		client = boto3.client('lambda', region_name=region_name) 
 		response = client.list_functions()
 		functions = response['Functions']		
-		if functions:  # because you may not have functions in every region
+		# Because you may not have functions in every region - If True:
+		if functions:  
 			count = 0
 			print 'The Region: %s ' % region_name
 			for item in functions:
 				count = count + 1
 				print count, item['FunctionName']
 			print "\n"
-count = 1
