c89387da0f63fc3d2822f30a1df259ffad1d5a0e
Rameezc <49646929+Rameezc@users.noreply.github.com>
2019-04-23 19:24:05 +0200

    Update List-Regions.py

--- List-Regions.py
+++ List-Regions.py
@@ -1,8 +1,13 @@
 import boto3
 
-ec2 = boto3.client('ec2')
-regions = ec2.describe_regions()
-print '\n' * 2
-for item in regions['Region']:
-  print 'Region --> : %s ' % item['RegionName']
+print 'Here we will be listing all the AWS Regions!'
+print
+
+def list_regions():
+    ec2 = boto3.client('ec2', region_name='us-east-1')
+    regions = ec2.describe_regions()
+    for item in regions['Regions']:
+        print 'Region ---> : %s ' % item['RegionName']
+
+list_regions()
 
