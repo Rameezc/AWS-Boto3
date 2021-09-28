8f757f9ff1dc22bb3a0e52130f8f237586a2ac75
Rameezc <49646929+Rameezc@users.noreply.github.com>
2021-09-28 12:17:49 +0200

    Update List_Lambda_Functions.py

--- List_Lambda_Functions.py
+++ List_Lambda_Functions.py
@@ -7,7 +7,7 @@ def list_myfunctions():
     ec2 = boto3.client('ec2', region_name='us-east-1')
     regions = ec2.describe_regions()
     for item in regions['Regions']:
-        print 'Region ---> : %s ' % item['RegionName']
+        print('Region ---> : %s ' % item['RegionName'])
         region_name = item['RegionName']
         # Ignore the following Region due to Permissions in my Account.
         if region_name != "ap-northeast-3":
@@ -18,15 +18,15 @@ def list_myfunctions():
             # If Function is True, Perform the following actions
             if functions:
                 count = 0
-                print "Please see the functions in the Region below : %s " % region_name
-                print "====================================================================="
+                print("Please see the functions in the Region below : %s " % region_name)
+                print ("=====================================================================")
                 for item in functions:
                     count += 1
-                    print count, item['FunctionName']
-                print '\n'
+                    print(count, item['FunctionName'])
+                print('\n')
             else:
-                print "This Region Has no Functions!"
-                print "==============================="
-                print '\n'
+                print("This Region Has no Functions!")
+                print("===============================")
+                print('\n')
 
 list_myfunctions()
